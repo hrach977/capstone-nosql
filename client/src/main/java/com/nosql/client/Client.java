7f1451512f8b84736e2bb9335715e96084aabdbe
hrach977 <hrachya.yeghishyan@picsart.com>
2019-10-31 15:45:07 +0400

    checkpoint

--- client/src/main/java/com/nosql/client/Client.java
+++ client/src/main/java/com/nosql/client/Client.java
@@ -37,6 +37,7 @@ public class Client {
             }
 
         });
+        this.readerThread.setName("Reader");
         this.readerThread.start();
     }
 
--- router/src/main/java/com/nosql/router/Router.java
+++ router/src/main/java/com/nosql/router/Router.java
@@ -1,14 +1,25 @@
 package com.nosql.router;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Router {
 
+    private final ServerSocket socket;
 
-    private final int port;
-
-    public Router(int port) {
-        this.port = port;
+    public Router(int port) throws IOException {
+        this.socket = new ServerSocket(port);
     }
 
     public void start() {
+        while (true) {
+            try {
+                Socket clientsSocket = this.socket.accept();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
